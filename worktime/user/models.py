60a9cb6ab3d37f0d986bfff7bf51670bc8f624a4
Iminokhun <boss151911@gmail.com>
2023-04-19 11:00:18 +0500

    changes

--- worktime/user/models.py
+++ worktime/user/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 class Organization(models.Model):
     name = models.CharField(max_length=255, verbose_name='Организация')
     stat_time = models.TimeField(blank=False)
     end_time = models.TimeField(blank=False)
+    qr_code = models.ImageField(verbose_name='QR код', blank=False)
 
     def __str__(self):
         return self.name
@@ -13,28 +14,26 @@ class Organization(models.Model):
         verbose_name = 'Организация'
         verbose_name_plural = 'Организации'
 
-class Employee(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
-    login = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
+class User(AbstractUser):
+    organization = models.OneToOneField(Organization, on_delete=models.CASCADE)
+    login = models.EmailField(unique=True,)
 
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+
+    # def __str__(self):
+    #     return f'{self.first_name} {self.last_name}'
 
     class Meta:
         verbose_name = 'Сотрудник'
         verbose_name_plural = 'Сотрудники'
 
 class WorkHours(models.Model):
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    date = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
     start_time = models.TimeField(auto_now_add=True, verbose_name='Время работы')
     end_time = models.TimeField(auto_now_add=True, verbose_name='Время оканчания')
     total_hours = models.FloatField(verbose_name='Обшее кол часов')
 
     def __str__(self):
-        return f'{self.start_time} - {self.end_time}'
+        return self.created_at
 
 
